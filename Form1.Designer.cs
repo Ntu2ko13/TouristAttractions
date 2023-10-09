4be0fb067a92e9bfd723205124b3454b7491a0d4
Ntuthuko Buthelezi <buthelezintuthuko1303@gmail.com>
2023-10-09 07:42:27 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TouristAttractions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblAttractions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(28, 71);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(111, 65);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit Guess";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(141, 179);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 65);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(199, 116);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(147, 20);
+            this.txtGuess.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(257, 179);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(104, 65);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblAttractions
+            // 
+            this.lblAttractions.AutoSize = true;
+            this.lblAttractions.Location = new System.Drawing.Point(224, 45);
+            this.lblAttractions.Name = "lblAttractions";
+            this.lblAttractions.Size = new System.Drawing.Size(57, 13);
+            this.lblAttractions.TabIndex = 4;
+            this.lblAttractions.Text = "Attractions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAttractions);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblAttractions;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TouristAttractions
+{
+    public partial class Form1 : Form
+    {
+        private string[] attractions = { "The Big Hole", "Table Mountain", "Kruger National Park", "Drakensberg Mountains" };
+        private string[] provinces = { "Northern Cape", "Western Cape", "Mpumalanga", "Kwazulu Natal" };
+
+        private int currentAttractionIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayNextAttraction();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string guess = txtGuess.Text.Trim();
+
+            if (currentAttractionIndex < attractions.Length)
+            {
+                string correctProvince = provinces[currentAttractionIndex];
+
+                if (guess.Equals(correctProvince, StringComparison.OrdinalIgnoreCase))
+                {
+                    // Correct province guess
+                    MessageBox.Show("Correct province guess! You've identified the province.");
+                    txtGuess.Clear();
+                    currentAttractionIndex++;
+
+                    if (currentAttractionIndex < attractions.Length)
+                    {
+                        DisplayNextAttraction();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Congratulations! You've completed the game.");
+                        Application.Exit();
+                    }
+                }
+                else
+                {
+                    // Incorrect province guess
+                    MessageBox.Show("Incorrect province guess. Try again.");
+                }
+            }
+        }
+        private void DisplayNextAttraction()
+        {
+            if (currentAttractionIndex < attractions.Length)
+            {
+                lblAttractions.Text = attractions[currentAttractionIndex];
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtGuess.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+
+
+
+
+
